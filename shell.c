e0edb2396e771bcb2d015e0a72c23bda507b134e
Daniel Lowry <dlowry909@gmail.com>
2020-01-24 11:03:57 +0000

    Add command tokenisation & add definitions
    Entered commands now tokenise and print the tokenised strings.
    
    Prompt changed from hard coded to definition (may change in the future),
    delimiter has also been made a definition for the tokenisation of the
    string.

--- shell.c
+++ shell.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #define STR_LIMIT 512
+#define PROMPT "> "
+#define DELIMITER " \t|><&;"
 
 int main() {
     char str[STR_LIMIT];
-    printf("> ");
+    printf(PROMPT);
     fgets(str, STR_LIMIT, stdin);
+    char* token = strtok(str, DELIMITER);
+
+    while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, DELIMITER);
+    }
 }
