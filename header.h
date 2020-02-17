c5eec892a013d1df3159f3543e1a2811bd3e45f4
Joe Stewart <joestewart47@googlemail.com>
2020-02-17 14:59:30 +0000

    Upadted header to match new funcs Loadhistory, addhistory, writehistorytofile

--- header.h
+++ header.h
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h> 
+#include <sys/stat.h>
 #define MAX_COMMAND_LENGTH 512
 #define MAX_TOK_NO 50
 #define TOK_DELIM " \t|><&;\n"
 #define DEBUG
+#define HistoryFile "./HistoryFile.txt"
 
 typedef struct {
     char *cwd;
@@ -23,6 +25,9 @@ int setPath(char* str);
 void changeDir(char* path, EnvVars *envVars);
 int execExternal(char **args);
 void execHistory(char **args, EnvVars *envVars);
+int write_history_tofile();
+int LoadHistory();
+void AddHistory(char *line);
 //Defining history structure
    int Hist_numb;
    char hist[20][512];
