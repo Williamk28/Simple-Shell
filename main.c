27165fb3ad33abdc89da77715366dd6c267ab21d
Daniel Lowry <dlowry909@gmail.com>
2020-02-20 15:25:13 +0000

    Fixed adding aliases to alias array
    --William is an idiot

--- main.c
+++ main.c
@@ -305,7 +305,7 @@ void exec_history(char **args, Env_vars *env_vars) {
   }  
 
   void addAlias(char **arg){
-    Alias_Struct Aliases [10];
+    //Alias_Struct Aliases [10];
     char aliasCommand[MAX_COMMAND_LENGTH];
     //Checks if the alias command is not empty
     if (arg[2] != NULL){
